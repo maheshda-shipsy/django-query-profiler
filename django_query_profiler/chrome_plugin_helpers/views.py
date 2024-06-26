d01e9dfeb7446b51c1c5869b4adcfc84aa8c4f7a
MaheshDA <mahesh.da@shipsy.io>
2024-06-26 22:42:49 +0530

    Context

--- django_query_profiler/chrome_plugin_helpers/views.py
+++ django_query_profiler/chrome_plugin_helpers/views.py
@@ -21,3 +21,13 @@ def get_query_profiled_data(request, redis_key: str, query_profiler_level: str) 
         'flamegraphStack': json.dumps(query_profiled_data.flamegraph_stack),
     }
     return render(request, QUERY_PROFILER_LEVEL_TO_TEMPLATE[query_profiler_level], context)
+
+def get_query_profiled_data_context(request, redis_key: str, query_profiler_level: str):
+    import pdb;pdb.set_trace()
+    query_profiled_data: QueryProfiledData = redis_utils.retrieve_data(redis_key)
+    context = {
+        'summary': query_profiled_data.summary,
+        'query_signature_to_statistics': query_profiled_data.query_signature_to_query_signature_statistics,
+        'flamegraphStack': json.dumps(query_profiled_data.flamegraph_stack),
+    }
+    return context
--- django_query_profiler/client/urls.py
+++ django_query_profiler/client/urls.py
@@ -6,4 +6,6 @@ GET_QUERY_PROFILED_DATA_NAME = 'get_query_profiled_data'
 urlpatterns = [
     path('<str:redis_key>/<str:query_profiler_level>', views.get_query_profiled_data,
          name=GET_QUERY_PROFILED_DATA_NAME),
+    path('context/<str:redis_key>/<str:query_profiler_level>', views.get_query_profiled_data_context,
+         name=GET_QUERY_PROFILED_DATA_NAME),
 ]
