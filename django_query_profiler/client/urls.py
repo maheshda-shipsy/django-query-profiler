c3c766577f8a03d906314f1c1e5d688b07bef252
MaheshDA <mahesh.da@shipsy.io>
2024-09-02 10:33:06 +0530

    Fixed Query Details URL

--- django_query_profiler/client/urls.py
+++ django_query_profiler/client/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from django_query_profiler.chrome_plugin_helpers import views
 
 GET_QUERY_PROFILED_DATA_NAME = 'get_query_profiled_data'
+GET_QUERY_PROFILED_DATA_JSON_NAME = 'get_query_profiled_data_json'
 urlpatterns = [
     path('<str:redis_key>/<str:query_profiler_level>', views.get_query_profiled_data,
          name=GET_QUERY_PROFILED_DATA_NAME),
     path('json_data/<str:redis_key>/<str:query_profiler_level>', views.get_n_plus1_query_data,
-         name=GET_QUERY_PROFILED_DATA_NAME),
+         name=GET_QUERY_PROFILED_DATA_JSON_NAME),
     path('query_performance/', views.query_performance_details, name='query_performance_details'),
 ]
